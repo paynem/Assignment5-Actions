752255fc5b8ab54e0ec7fba987eb34799f8cb4bc
paynem <mpayne677@gmail.com>
2020-02-28 11:53:08 -0800

    dateTime test v3

--- task.py
+++ task.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+
 def firstrun():
     return "success"
 
@@ -12,6 +13,7 @@ def listInfo(theList):
     first = theList[0]
     last = theList[-1]
     return first, last
+
 
 def dateTime(a, b):
     return a - b
